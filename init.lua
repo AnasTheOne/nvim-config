f96ea7a2a717c9f73f302ff20aa35e5dcfecd9f6
anas.elmoussaoui <anas.elmoussaoui@visier.com>
2025-05-12 11:58:50 +0200

    add comment

--- init.lua
+++ init.lua
@@ -1,5 +1,7 @@
 -- bootstrap lazy.nvim, LazyVim and your plugins
 require("config.lazy")
+
+-- Getting rid of the padding around nvim when the background color of the terminal is different than the one in nvim
 vim.api.nvim_create_autocmd({ "UIEnter", "ColorScheme" }, {
   callback = function()
     local normal = vim.api.nvim_get_hl(0, { name = "Normal" })
